f88bc2b1ea02d78bb4482956744e2737c5929e24
AYOUB <a.idihia@gmail.com>
2023-04-20 16:32:36 +0200

    first step

---
+++ src/main/java/io/awiya/servermanagment/ServerManagmentApplication.java
@@ -0,0 +1,13 @@
+package io.awiya.servermanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/awiya/servermanagment/ServerManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.awiya.servermanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
