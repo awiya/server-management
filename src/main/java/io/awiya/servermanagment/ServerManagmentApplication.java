832802fd55c5c87791771b7184858f88a716fdfd
AYOUB <a.idihia@gmail.com>
2023-04-23 11:36:37 +0200

    Some changes

--- src/main/java/io/awiya/servermanagment/ServerManagmentApplication.java
+++ src/main/java/io/awiya/servermanagment/ServerManagmentApplication.java
@@ -3,11 +3,16 @@ package io.awiya.servermanagment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class ServerManagmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerManagmentApplication.class, args);
 	}
+
+
+
 
 }
